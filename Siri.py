993fd9d91fa69ff758504781a74f2400d5ca90d3
Umanda <umandaravimal@gmail.com>
2023-11-25 00:10:10 +0530

    date function

--- Siri.py
+++ Siri.py
@@ -12,4 +12,10 @@ def time():
     Time = datetime.datetime.now().strftime("%I:%M:%S")
     speak(Time)
 
-time()
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    date = int(datetime.datetime.now().day)
+    speak(date)
+    speak(month)
+    speak(year)
